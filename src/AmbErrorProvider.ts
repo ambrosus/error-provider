50077561c2743785f7e3125b7ba732f761cde893
SvineruS <37367870+SvineruS@users.noreply.github.com>
2024-01-08 14:42:47 +0200

    Update AmbErrorProvider.ts

--- src/AmbErrorProvider.ts
+++ src/AmbErrorProvider.ts
@@ -1,5 +1,39 @@
 import { BigNumber, ethers } from "ethers";
 
+export async function errorByTxHash(txHash: string, provider: ethers.providers.Provider) {
+  const tx = await provider.getTransaction(txHash);
+  if (!tx) throw new Error(`Transaction ${txHash} not found`);
+
+  return await provider.call(
+    { data: tx.data, value: tx.value, from: tx.from, to: tx.to, gasLimit: tx.gasLimit, gasPrice: tx.gasPrice },
+    tx.blockNumber
+  );
+}
+
+export function wrapProviderToError(provider: ethers.providers.Provider) {
+  const oldCall = provider.call;
+  provider.call = async (transaction: any, blockTag?: any): Promise<string> => {
+    try {
+      return await oldCall.apply(provider, [transaction, blockTag]);
+    } catch (e) {
+      // throw e;
+      throw parseError(e);
+    }
+  };
+
+  const oldEstimateGas = provider.estimateGas;
+  provider.estimateGas = async (transaction: any): Promise<BigNumber> => {
+    try {
+      return await oldEstimateGas.apply(provider, [transaction]);
+    } catch (e) {
+      await provider.call(transaction); // make callStatic request to get error text
+      throw e; // shouldn't get here because callStatic should throw amb error
+    }
+  };
+
+  return provider;
+}
+
 export class AmbErrorProviderWeb3 extends ethers.providers.Web3Provider {
   // Populates "from" if unspecified, and estimates the gas for the transaction
   async estimateGas(transaction: any): Promise<BigNumber> {
@@ -58,15 +92,25 @@ function _parseError(error: any): any {
   if (error.code === "SERVER_ERROR") error = error.error;
   if (error.code === -32603) error = error.data;
 
-  if (error.code !== -32015) throw "Not -32015 code";
-  if (!error.data.startsWith("Reverted 0x")) throw "Not starts with Reverted 0x";
+  if (error.code !== -32015) {
+    console.warn("Not -32015 code", error);
+    throw error;
+  }
+  if (!error.data.startsWith("Reverted 0x")) {
+    console.warn("Not starts with Reverted 0x", error);
+    throw error;
+  }
 
   let reason = error.data.substring(9);
   // https://github.com/authereum/eth-revert-reason/blob/e33f4df82426a177dbd69c0f97ff53153592809b/index.js#L93
   // "0x08c379a0" is `Error(string)` method signature, it's called by revert/require
-  if (reason.length < 138 || !reason.startsWith("0x08c379a0")) throw "Not error signature";
+  if (reason.length < 138 || !reason.startsWith("0x08c379a0")) {
+    console.warn("Not error signature", reason);
+    throw error;
+  }
 
   reason = ethers.utils.hexDataSlice(reason, 4);
   reason = ethers.utils.defaultAbiCoder.decode(["string"], reason);
+  console.warn("Error provider successfully parse error: ", reason);
   return new Error(reason);
 }
